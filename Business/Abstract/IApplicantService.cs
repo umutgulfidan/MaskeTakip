54134f20c5ddbb4b4e1048dc00eee2878eef2a18
umutgulfidan <umutgulfidan@gmail.com>
2024-02-11 12:17:34 +0300

    Proje dosyası ekle.

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IApplicantService
+    {
+        public void ApplyForMask(Person person);
+
+        public List<Person> GetAll();
+
+        public bool CheckPerson(Person person);
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+
+PttManager pttManager = new PttManager(new PersonManager());
+Person person = new Person();
+
+person.NationalIdentity = 12345678901;
+person.FirstName ="Test";
+person.LastName = "Test";
+person.DateOfBirthYear = 1994;
+
+pttManager.GiveMask(person);
+
